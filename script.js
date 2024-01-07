575bc1ee339d7d222e91e02e2f54a11de1be967e
olemarne <133375019+olemarne@users.noreply.github.com>
2024-01-07 19:10:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Assigning elements from html to js variables
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
